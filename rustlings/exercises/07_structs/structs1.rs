dc52593e8174f1ae1e441e95603dcd7f56a76f8b
Veethikyn <ziqinwang1@gmail.com>
2024-08-11 22:41:12 +0800

    for the enum

--- rustlings/exercises/07_structs/structs1.rs
+++ rustlings/exercises/07_structs/structs1.rs
@@ -53,6 +53,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
         // let unit_struct =
+        let unit_struct = UniStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
